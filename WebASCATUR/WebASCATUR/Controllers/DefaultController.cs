f8ce165f6abd3646532e4ce064751c70f45cfdbc
manuel vargas vega <mavarg07@gmail.com>
2017-12-10 11:03:12 -0600

    Se agrega el trabajo de ejemplo con vistas parciales

---
+++ WebASCATUR/WebASCATUR/Controllers/DefaultController.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using WebASCATUR.Models;
+using WebASCATUR.ViewModels;
+
+namespace WebASCATUR.Controllers
+{
+    public class DefaultController : Controller
+    {
+        private readonly AdmWebASCATUR_Default_v1Context _context;
+
+        public DefaultController(AdmWebASCATUR_Default_v1Context context)
+        {
+            _context = context;
+        }
+        // GET: Default
+        public ActionResult Index()
+        {
+            OpinionListViewModel vm = new OpinionListViewModel();
+            vm.Message = "Hello from DefaultController.Index()";
+            vm.Opiniones = _context.Opinion.ToList();
+
+            return View(vm);
+          
+        }
+
+        // GET: Default/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: Default/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Default/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(IFormCollection collection)
+        {
+            try
+            {
+                // TODO: Add insert logic here
+
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Default/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: Default/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(int id, IFormCollection collection)
+        {
+            try
+            {
+                // TODO: Add update logic here
+
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Default/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: Default/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id, IFormCollection collection)
+        {
+            try
+            {
+                // TODO: Add delete logic here
+
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
---
+++ WebASCATUR/WebASCATUR/Controllers/ListaOpinionesController.cs
@@ -0,0 +1,159 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using WebASCATUR.Models;
+
+namespace WebASCATUR.Controllers
+{
+    public class ListaOpinionesController : Controller
+    {
+        private readonly AdmWebASCATUR_Default_v1Context _context;
+
+        public ListaOpinionesController(AdmWebASCATUR_Default_v1Context context)
+        {
+            _context = context;
+        }
+
+        // GET: ListaOpiniones
+        public ActionResult Index()
+        {
+            IEnumerable<Opinion> opiniones;
+            opiniones = _context.Opinion.ToList();
+            return View(opiniones);
+        }
+        // GET: ListaOpiniones/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var opinion = await _context.Opinion
+                .Include(o => o.IdComercioNavigation)
+                .SingleOrDefaultAsync(m => m.Id == id);
+            if (opinion == null)
+            {
+                return NotFound();
+            }
+
+            return View(opinion);
+        }
+
+        // GET: ListaOpiniones/Create
+        public IActionResult Create()
+        {
+            ViewData["IdComercio"] = new SelectList(_context.Comercio, "Id", "Cedula");
+            return View();
+        }
+
+        // POST: ListaOpiniones/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Id,IdComercio,Detalle,NombreUsuario,FechaIngreso")] Opinion opinion)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(opinion);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["IdComercio"] = new SelectList(_context.Comercio, "Id", "Cedula", opinion.IdComercio);
+            return View(opinion);
+        }
+
+        // GET: ListaOpiniones/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var opinion = await _context.Opinion.SingleOrDefaultAsync(m => m.Id == id);
+            if (opinion == null)
+            {
+                return NotFound();
+            }
+            ViewData["IdComercio"] = new SelectList(_context.Comercio, "Id", "Cedula", opinion.IdComercio);
+            return View(opinion);
+        }
+
+        // POST: ListaOpiniones/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("Id,IdComercio,Detalle,NombreUsuario,FechaIngreso")] Opinion opinion)
+        {
+            if (id != opinion.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(opinion);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!OpinionExists(opinion.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["IdComercio"] = new SelectList(_context.Comercio, "Id", "Cedula", opinion.IdComercio);
+            return View(opinion);
+        }
+
+        // GET: ListaOpiniones/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var opinion = await _context.Opinion
+                .Include(o => o.IdComercioNavigation)
+                .SingleOrDefaultAsync(m => m.Id == id);
+            if (opinion == null)
+            {
+                return NotFound();
+            }
+
+            return View(opinion);
+        }
+
+        // POST: ListaOpiniones/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var opinion = await _context.Opinion.SingleOrDefaultAsync(m => m.Id == id);
+            _context.Opinion.Remove(opinion);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool OpinionExists(int id)
+        {
+            return _context.Opinion.Any(e => e.Id == id);
+        }
+    }
+}
